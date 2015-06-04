02d5ee677c622bb571090f829001c4936a5cd2da
andreasbloomquist <andreasbloomquist@gmail.com>
2015-06-04 07:38:51 -0700

    Race car code

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,12 @@
 class Car
-  #write Car class code here
+  attr_accessor :speed
+
+  def initialize
+    self.speed = 0
+  end
+
+  def accelerate set_speed
+    self.speed += set_speed
+  end
+
 end
--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,25 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+  @@cars = []
+  def intialize
+    cars
+  end
+
+  def cars
+    car1 = Car.new
+    car2 = Car.new
+    car1.accelerate(rand(100))
+    car2.accelerate(rand(100))
+    @@cars.push(car1, car2)
+
+  end
+
+  def winner
+    self.cars[0].speed > self.cars[1].speed ? self.cars[0] : self.cars[1]
+  end
+
+  def loser
+    winner == self.cars[0] ? self.cars[1] : self.cars[0]
+  end
 end
